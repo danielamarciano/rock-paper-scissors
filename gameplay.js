a835db6954c458f9ea6b4dd8bbc2eb997aa48921
danielamarciano <danielalmarciano@gmail.com>
2022-12-01 21:12:34 -0500

    Edit function to announce winner

--- gameplay.js
+++ gameplay.js
@@ -5,68 +5,68 @@ const div = document.querySelector('div')
 
 
 // Create function to select random choice for computer
-function getComputerChoice (){
-    return choices[Math.floor(Math.random()*choices.length)]
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)]
 }
 
 let playerScore = 0
 let computerScore = 0
 // Create function to play rounds and display round text
-function playRound(playerSelection, computerSelection){
-  if  ((playerSelection == "rock" && computerSelection == 'scissors') ||
+function playRound(playerSelection, computerSelection) {
+    if ((playerSelection == "rock" && computerSelection == 'scissors') ||
         (playerSelection == "scissors" && computerSelection == 'paper') ||
-        (playerSelection == "paper" && computerSelection == 'rock')){
-        
+        (playerSelection == "paper" && computerSelection == 'rock')) {
+
         result = (`You win! ${playerSelection} beats ${computerSelection}.`);
         playerScore += 1
-        } 
+    }
 
-    else if (playerSelection === computerSelection){
+    else if (playerSelection === computerSelection) {
         result = (`You tie! You both selected ${playerSelection}.`)
-        } 
-   
+    }
+
     else {
         result = (`You lose!  ${computerSelection} beats ${playerSelection}.`);
         computerScore += 1
-        } 
-    
-return result
+    }
+
+    return result
 
 }
 
-function scoreboard(){
-   playRound(playerSelection,computerSelection);
-   if (playerScore>computerScore){
-   console.log("Player Won");
+function scoreboard() {
+    if (playerScore > computerScore) {
+        console.log("Player Won!");
     }
-   if (computerScore>playerScore) {
-   console.log("Computer Won");
+    if (computerScore > playerScore) {
+        console.log("Computer Won, darn!");
     }
-  }
+}
 
-function game(){
-    for (let i=0; i < 4; i++){
+function game() {
+    if (playerScore == 5 || computerScore == 5)
         scoreboard()
-} return [playerScore, computerScore]
-
+    return [playerScore, computerScore]
 }
-
 
 
 // Create listener event on button to playRound when button is pressed
 buttons.forEach((button) => {
     button.addEventListener('click', (e) => {
-         playerSelection = button.id;
-         computerSelection = getComputerChoice()
-         const container = document.querySelector('.container')
-         container.textContent = playRound(playerSelection, computerSelection) 
+        playerSelection = button.id;
+        computerSelection = getComputerChoice()
+        const container = document.querySelector('.container')
+        container.textContent = playRound(playerSelection, computerSelection)
 
-         const score = document.querySelector('.score')
-         score.textContent = game()
+        const score = document.querySelector('.score')
+        score.textContent = game()
 
-       
-  })});
 
-// Display the running score, and announce a winner of 
-//the game once one player reaches 5 points.
 
+
+    })
+});
+
+
+// after someone gets to 5, winner announced
+// after 5 points reached, next points clears both to 0 0 and add point
